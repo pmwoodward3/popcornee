37d90a6214b9434ee277db45696f48ad82713607
Armin Monirzadeh <arm1n3m73@gmail.com>
2016-10-10 10:54:29 +0330

    sucked data

--- app/controllers/yts_controller.rb
+++ app/controllers/yts_controller.rb
@@ -4,19 +4,41 @@ require 'net/http'
 require 'ostruct'
 
 class YtsController < ApplicationController
-    $ytsBaseURI = "https://yts.ag/api/v2/list_movies.json?limit=50"
-    def extractor
-        if params[:q]
-            query = "&query_term=" + params[:q].parameterize('+')
-            ytsURI = $ytsBaseURI + query
-        else
-            ytsURI = $ytsBaseURI + "&query_term=2016"
-        end
-        response = Net::HTTP.get_response(URI.parse(ytsURI))
+    def index
+        @movies = Movie.all.page(params[:page]).per(10)
+    end
 
+    def suckyts
+        ytsURI = "https://yts.ag/api/v2/list_movies.json?limit=50&page=1"
+        response = Net::HTTP.get_response(URI.parse(ytsURI))
         results = JSON.parse(response.body)["data"]["movies"]
-        if results
-            @result = results.uniq
+        @result = results
+        $i = 2
+        $num = 117
+        while $i < $num  do
+            ytsURI = "https://yts.ag/api/v2/list_movies.json?limit=50&page=" + $i.to_s
+            response = Net::HTTP.get_response(URI.parse(ytsURI))
+            results = JSON.parse(response.body)["data"]["movies"]
+            @result = @result | results  
+            $i += 1
         end
+        
+        @result.each do |nyts|
+            @movie = Movie.new
+            @movie.title = nyts["title"]
+            @movie.year = nyts["year"]
+            @movie.rating = nyts["rating"]
+            @movie.runtime = nyts["runtime"]
+            @movie.genres = nyts["genres"]
+            @movie.summary = nyts["summary"]
+            @movie.language = nyts["language"]
+            @movie.poster = nyts["large_cover_image"]
+            @movie.background = nyts["background_image"]
+            @movie.imdb = nyts["imdb_code"]
+            @movie.torrents = nyts["torrents"]
+            @movie.save
+        end
+        
+        @value = Movie.count
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
     resources :yts
-    root "yts#extractor"
+    root "yts#index"
 end
