cf12415a11f1124fcb9953d753aa228437ac4579
Armin Monirzadeh <arm1n3m73@gmail.com>
2016-10-17 10:23:44 +0330

    route config fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-    match '*path' => redirect('/')   unless Rails.env.development?
     get "static/:p" => "static#show"
     resources :movies, :path => ''
     root "movies#index"
